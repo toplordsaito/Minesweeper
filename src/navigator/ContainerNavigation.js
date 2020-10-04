9c037d9658c35b8b6fe14c454ad6438a279be9dc
Jirat Thongpeng <42910435+it61070027@users.noreply.github.com>
2020-10-04 11:35:29 +0700

    tutorial

--- src/navigator/ContainerNavigation.js
+++ src/navigator/ContainerNavigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from '../scenes/Home.js';
 import Offline from '../scenes/Offline.js';
 import test from '../scenes/gamedemo.js'
+import Tutorial from '../scenes/tutorial'
 import  {createStackNavigator}  from '@react-navigation/stack';
 function ContainerNavigation() {
     const Stack = createStackNavigator();
@@ -12,6 +13,7 @@ function ContainerNavigation() {
           <Stack.Screen name="Home" component={Home}  options={{title: '', headerTransparent: true}}/>
           <Stack.Screen name="Offline" component={Offline} />
           <Stack.Screen name="OfflineGame" component={test}  options={{title: '', headerTransparent: true}} />
+          <Stack.Screen name="Tutorial" component={Tutorial} />
         </Stack.Navigator>
       </NavigationContainer>
     );
