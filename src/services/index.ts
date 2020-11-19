fb902dfba54db2ef4715ebbeb671d87c1236eeb4
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2020-11-19 15:39:54 +0700

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,14 +2,7 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBp-WQGVKw4opmwuHteFpihZOpg631Yd30",
-  authDomain: "minesweeper-cf877.firebaseapp.com",
-  databaseURL: "https://minesweeper-cf877.firebaseio.com",
-  projectId: "minesweeper-cf877",
-  storageBucket: "minesweeper-cf877.appspot.com",
-  messagingSenderId: "753300011946",
-  appId: "1:753300011946:web:f3b61e7aa5d9704dc0c9d3",
-  measurementId: "G-NKD41Z1J9H"
+  
 };
 
 if (!firebase.apps.length) {
