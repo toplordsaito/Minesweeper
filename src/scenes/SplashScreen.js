408dcaf3d4a0488178474192fa42a020f807579c
Jirat Thongpeng <42910435+it61070027@users.noreply.github.com>
2020-11-22 03:27:53 +0700

    ani

--- src/scenes/SplashScreen.js
+++ src/scenes/SplashScreen.js
@@ -1,42 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { View, Image } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import { CommonActions } from '@react-navigation/native';
-import styles from '../styles/splash.styles';
-import stylesTheme from '../styles/theme.styles';
+import React, { useEffect, useRef } from "react";
+import { View, Image, Animated } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { CommonActions } from "@react-navigation/native";
+import styles from "../styles/splash.styles";
+import stylesTheme from "../styles/theme.styles";
 import { Text } from "react-native-elements";
 const SplashScreen = ({ navigation }) => {
+  const springVal = useRef(new Animated.Value(0.5)).current;
+  const spring = () => {
+    Animated.spring(springVal, {
+      toValue: 1,
+      friction: 1,
+    }).start(() => {
+      springVal.setValue(1);
+    });
+  };
   useEffect(() => {
+    spring()
     const checkStorage = async () => {
-    const user = await AsyncStorage.getItem('login');
-      if (user){
+      const user = await AsyncStorage.getItem("login");
+      if (user) {
         setTimeout(function () {
           navigation.dispatch(
             CommonActions.reset({
-              routes: [{ name: 'Home' }],
-            }),
+              routes: [{ name: "Home" }],
+            })
           );
-        }, 2500);
-      }
-      else{
+        }, 3000);
+      } else {
         setTimeout(function () {
           navigation.dispatch(
             CommonActions.reset({
-              routes: [{ name: 'Login' }],
-            }),
+              routes: [{ name: "Login" }],
+            })
           );
-        }, 2500);
+        }, 3000);
       }
-  }
-  checkStorage()
+    };
+    checkStorage();
   }, []);
-
 
   return (
     <View style={stylesTheme.container}>
       <View style={styles.logoContainer}>
-        <Image source={require('../asset/logo.png')} style={styles.logo} />
-        <Text style={stylesTheme.text} h1>M<Text style={{ color: "red" }}>i</Text>neSweeper</Text>
+        <Animated.Image source={require("../asset/logo.png")} style={[styles.logo, { transform: [{scale: springVal}]}]} />
+        <Text style={stylesTheme.text} h1>
+          M<Text style={{ color: "red" }}>i</Text>neSweeper
+        </Text>
       </View>
     </View>
   );
