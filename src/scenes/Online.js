b6f5aa3df935d138dd1f05cf07457e20f51278ff
Jirat Thongpeng <42910435+it61070027@users.noreply.github.com>
2020-11-23 20:06:44 +0700

    ปรับ

--- src/scenes/Online.js
+++ src/scenes/Online.js
@@ -2,29 +2,26 @@ import React, { Component } from "react";
 import { Button, Text } from "react-native-elements";
 import { StyleSheet, View } from "react-native";
 import { Picker } from "@react-native-community/picker";
-import { useCreateRoom } from '../hooks'
-const MODE = ['PvP', 'Ranking', 'Battle Royal', 'Any'];
+import { useCreateRoom } from "../hooks";
+const MODE = ["PvP", "Ranking", "Battle Royal", "Any"];
 
 const CreateRoomButton = ({ createRoomHanler }) => {
-  const { createRoom, isCreatingRoom } = useCreateRoom()
+  const { createRoom, isCreatingRoom } = useCreateRoom();
   const handleCreateRoom = async () => {
-    const roomId = await createRoom()
-    console.log("roomId", roomId)
-    createRoomHanler(roomId)
-  }
-  return <Button
-    style={styles.button}
-    title={"Create"}
-    onPress={handleCreateRoom}
-  />
-}
-
+    const roomId = await createRoom();
+    console.log("roomId", roomId);
+    createRoomHanler(roomId);
+  };
+  return (
+    <Button style={styles.button} title={"Create"} onPress={handleCreateRoom} />
+  );
+};
 
 export default class Online extends Component {
   state = {
     currentValue: 0,
-    currentMode: 'PvP',
-  }
+    currentMode: "Ranking",
+  };
   constructor(props) {
     super(props);
   }
@@ -33,44 +30,45 @@ export default class Online extends Component {
     this.setState({
       currentValue: currentValue,
       currentMode: currentMode,
-    })
+    });
     // console.log(this.state.currentValue);
     // console.log(this.state.currentMode);
-
-  }
+  };
 
   createRoomHanler = async (roomid) => {
-    console.log(roomid)
+    console.log(roomid);
     this.props.navigation.navigate("Lobby", {
       value: this.state.currentValue,
-      role: 'owner',
+      role: "owner",
       code: roomid,
-    })
-    
-  }
+    });
+  };
   renderMode = () => {
     return (
       <View style={styles.container}>
-        <Text h3 style={{ color: "white" }}>MOD<Text style={{ color: "red" }}>E</Text> :</Text>
-        <Picker
-          style={styles.picker}
-          itemStyle={{ height: 100, color: "white" }}
-          selectedValue={this.state.currentMode}
-          onValueChange={(mode) => this.changeMode(mode)}
-        >
-          <Picker.Item label="PvP" value="PvP" />
-          <Picker.Item label="Ranking" value="Ranking" />
-          <Picker.Item label="Battle Royal" value="Battle Royal" />
-          <Picker.Item label="Any" value="Any" />
-        </Picker>
+        <Text h3 style={{ color: "white" }}>
+          MOD<Text style={{ color: "red" }}>E</Text> :
+        </Text>
+        <View style={Platform.OS === "android" ? styles.pickerAndriod : null}>
+          <Picker
+            dropdownIconColor="red"
+            style={styles.picker}
+            itemStyle={{ height: 100, color: "white" }}
+            selectedValue={this.state.currentMode}
+            onValueChange={(mode) => this.changeMode(mode)}
+          >
+            <Picker.Item label="Ranking" value="Ranking" />
+            <Picker.Item label="Battle Royal" value="Battle Royal" />
+          </Picker>
+        </View>
       </View>
-    )
-  }
+    );
+  };
   render() {
     return (
       <View style={[styles.container, { backgroundColor: "#212930" }]}>
         {this.renderMode()}
-        <View style={{ flex: 1, width: '80%' }}>
+        <View style={{ flex: 1, width: "80%" }}>
           {/* <Button
             style={styles.button}
             title={"Create"}
@@ -89,7 +87,7 @@ export default class Online extends Component {
           />
         </View>
       </View>
-    )
+    );
   }
 }
 
@@ -102,9 +100,16 @@ const styles = StyleSheet.create({
   button: {
     margin: 2,
   },
+  pickerAndriod: {
+    marginTop: "10%",
+    borderWidth: 2,
+    borderColor: "#c2c2c1",
+    borderRadius: 5,
+    backgroundColor: "white",
+  },
   picker: {
     height: 50,
     width: 200,
     alignSelf: "center",
-  }
-})
+  },
+});
