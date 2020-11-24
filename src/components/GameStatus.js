183faec52a21a0dfcce74c06b63b7f82c01dac8d
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2020-11-24 23:26:12 +0700

    Update GameStatus.js

--- src/components/GameStatus.js
+++ src/components/GameStatus.js
@@ -7,58 +7,58 @@ import { useSelector } from "react-redux";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const GameStatus = ({ code }) => {
-  const { isFetching, game } = useGame(code)
-  const colorData = useSelector((state) => state.theme.colorData);
-  const text = {color: colorData.text, fontFamily: colorData.fontFamily};
-  console.log(game)
-  if (!game) {
-    return (<Text>Loading . . .</Text>)
-  } else {
-    return (
-      <View style={[stylesTheme.logoContainer, {width: wp("100%"), height: hp("25%"),backgroundColor: colorData.backgroundColor}]}>
-        <View style={[stylesTheme.logoContainer, {height: hp("15%")}]}>
-          <Text style={[stylesTheme.headerText, text]} h2>
-            S<Text style={{color: colorData.innerText}}>t</Text>atus <Text style={{color: colorData.innerText}}>G</Text>ame
+    const { isFetching, game } = useGame(code)
+    const colorData = useSelector((state) => state.theme.colorData);
+    const text = { color: colorData.text, fontFamily: colorData.fontFamily };
+    console.log(game)
+    if (!game) {
+        return (<Text>Loading . . .</Text>)
+    } else {
+        return (
+            <View style={[stylesTheme.logoContainer, { width: wp("100%"), height: hp("25%"), backgroundColor: colorData.backgroundColor }]}>
+                <View style={[stylesTheme.logoContainer, { height: hp("15%") }]}>
+                    <Text style={[stylesTheme.headerText, text]} h2>
+                        S<Text style={{ color: colorData.innerText }}>t</Text>atus <Text style={{ color: colorData.innerText }}>G</Text>ame
           </Text>
-        </View>
-        <View style={{height: hp("10%")}}>
-          {
-            game.result.map((l, i) => {
-              // console.log(l.id)
-              // console.log(l?.name)
-              return (
-                <View>
-                  <Text style={[styles.textLog, text]}>Rank : {i + 1}{"\n"}Name : {l.name} <Text style={{color: colorData.innerText}}>{l.status}</Text></Text>
                 </View>
-              )
-            })
-          }
-        </View>
-      </View>
-    )
-  }
+                <View style={{ height: hp("10%") }}>
+                    {
+                        game.result.map((l, i) => {
+                            // console.log(l.id)
+                            // console.log(l?.name)
+                            return (
+                                <View key={i}>
+                                    <Text style={[styles.textLog, text]}>Rank : {i + 1}{"\n"}Name : {l.name} <Text style={{ color: colorData.innerText }}>{l.status}</Text></Text>
+                                </View>
+                            )
+                        })
+                    }
+                </View>
+            </View>
+        )
+    }
 }
 export default GameStatus
 
 const styles = StyleSheet.create({
-  container: {
-  flex: 0.2,
-  alignContent: "center",
-  marginBottom: "15%"
+    container: {
+        flex: 0.2,
+        alignContent: "center",
+        marginBottom: "15%"
 
-  },
-  textLog: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  button: {
-  margin: 2,
-  },
-  textInput: {
-  height: 40,
-  margin: 2,
-  backgroundColor: 'white',
-  textAlign: 'center',
-  borderRadius: 5,
-  }
+    },
+    textLog: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    button: {
+        margin: 2,
+    },
+    textInput: {
+        height: 40,
+        margin: 2,
+        backgroundColor: 'white',
+        textAlign: 'center',
+        borderRadius: 5,
+    }
 })
